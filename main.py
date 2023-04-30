8b8a2a77835144e5492b5da473eb37c345cd6a0b
xqxEren <121521831+xqxEren@users.noreply.github.com>
2023-04-30 22:45:30 +0300

    odev1
    odev1

---
+++ main.py
@@ -0,0 +1,18 @@
+islami_gün_sayisi: int = int(input("Lütfen adını öğrenmek istediğiniz günün numarasını girin: "))
+
+if islami_gün_sayisi == 1:
+    print("Yevmu’l-İs̠neyn")
+elif islami_gün_sayisi == 2:
+    print("Yevmu’s̠-S̠ülās̠ā’")
+elif islami_gün_sayisi == 3:
+    print("Yevmu’l-Erbi‘ā’")
+elif islami_gün_sayisi == 4:
+    print("Yevmu’l-Hamīs")
+elif islami_gün_sayisi == 5:
+    print("Yevmu’l-Cumu'a")
+elif islami_gün_sayisi == 6:
+    print("Yevmu’s-Sebt")
+elif islami_gün_sayisi == 7:
+    print("Yevmu’l-Eḥad")
+else:
+    print("Yanlış bir değer girdiniz !!")
