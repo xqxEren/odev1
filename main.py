1b6b427b14aafaab8ff7d97c8ea2b7a83a407f5f
xqxEren <121521831+xqxEren@users.noreply.github.com>
2023-04-30 22:50:51 +0300

    odev2
    odev2

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-islami_gün_sayisi: int = int(input("Lütfen adını öğrenmek istediğiniz günün numarasını girin: "))
+islami_isim: int = int(input("Lütfen adını öğrenmek istediğiniz islam ismini girin: "))
 
-if islami_gün_sayisi == 1:
-    print("Yevmu’l-İs̠neyn")
-elif islami_gün_sayisi == 2:
-    print("Yevmu’s̠-S̠ülās̠ā’")
-elif islami_gün_sayisi == 3:
-    print("Yevmu’l-Erbi‘ā’")
-elif islami_gün_sayisi == 4:
-    print("Yevmu’l-Hamīs")
-elif islami_gün_sayisi == 5:
-    print("Yevmu’l-Cumu'a")
-elif islami_gün_sayisi == 6:
-    print("Yevmu’s-Sebt")
-elif islami_gün_sayisi == 7:
-    print("Yevmu’l-Eḥad")
+if islami_isim == 1:
+    print("Muharrem")
+elif islami_isim == 2:
+    print("Safer")
+elif islami_isim == 3:
+    print("Rebiülevvel")
+elif islami_isim == 4:
+    print("Rebiülahir")
+elif islami_isim == 5:
+    print("Cemaziyelevvel")
+elif islami_isim == 6:
+    print("Cemaziyelahir")
+elif islami_isim == 7:
+    print("Recep")
+elif islami_isim == 8:
+    print("Şaban")
+elif islami_isim == 9:
+    print("Ramazan")
+elif islami_isim == 10:
+    print("Şevval")
+elif islami_isim == 11:
+    print("Zilkade")
+elif islami_isim == 12:
+    print("Zilhicce")
 else:
     print("Yanlış bir değer girdiniz !!")
